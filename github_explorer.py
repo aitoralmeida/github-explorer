be6fa9d1754296dbb0ccc95feb9a3c8539ab3c3f
Aitor Almeida <aitoralmeida@gmail.com>
2014-02-14 14:01:34 +0100

    added method to create the follower/followee network

--- github_explorer.py
+++ github_explorer.py
@@ -14,15 +14,15 @@ def get_token():
     with open('token.txt', 'r') as tokenfile:
         return tokenfile.readline()
         
-def get_repos_names(user):
+def get_repos_names(user_login):
     gh = Github(token, per_page=100)
-    user = gh.get_user(user)
+    user = gh.get_user(user_login)
     repo_names = [repo.name for repo in user.get_repos()]
     return repo_names
     
-def get_stargazers(user):
+def get_stargazers(user_login):
     gh = Github(token, per_page=100)
-    user = gh.get_user(user)
+    user = gh.get_user(user_login)
     stargazers = set()
     for repo in user.get_repos():
       for s in repo.get_stargazers():
@@ -30,12 +30,29 @@ def get_stargazers(user):
       
     return stargazers
     
-
+def build_followers_network(targets):
+    G = nx.DiGraph()
+    gh = Github(token, per_page=100)
+    for target in targets:
+        print 'Processing', target
+        try:
+            user = gh.get_user(target)
+            for follower in user.get_followers():
+                G.add_edge(follower.login, user.login)
+            for followee in user.get_following():
+                G.add_edge(user.login, followee.login)
+        except:
+            print 'User does not exists',target
+            
+    return G
         
 
     
         
 if __name__=='__main__':
     token = get_token()
-#   print get_repos_names('aitoralmeida')
-    print get_stargazers('aitoralmeida')
+    G = build_followers_network(['aitoralmeida', 'porduna', 'OscarPDR', 'jonlazaro', 'juansixto', 'memaldi', 'unaguil', 'gomezgoiri', 'edlectrico', 'josubg'])
+    print len(G.nodes())
+    print len(G.edges())
+    nx.write_gexf(G,'github.gexf')
+    print 'done'
